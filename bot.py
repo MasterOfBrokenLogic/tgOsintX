f04e7efe8a69aca415e9c1293b48975283d8380b
DrazeForce <drazeforce.io@gmil.com>
2026-03-10 20:32:06 +0530

    Added new variable to the environment

--- bot.py
+++ bot.py
@@ -904,6 +904,9 @@ def main():
     threading.Thread(target=startSelfPing, daemon=True).start()
 
     log.info("Bot running")
+    import asyncio
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
